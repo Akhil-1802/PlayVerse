b26884d6f116061fcb6517189c8b9f3a86cdc1bd
Akhil-1802 <akhilmaindola18@gmail.com>
2026-03-29 19:43:31 +0530

    room creation issue resolved

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const apiClient = axios.create({
-    baseURL : "http://localhost:3000",
+    baseURL : "https://playverse-bg0l.onrender.com",
     headers : {
         "Content-type" : "application/json"
     }
